689dd282f8029341442ac89be8289c3cc00f419b
paulbinot <paulbinot23@gmail.com>
2022-11-07 14:32:56 +0100

    index and pokemon page with style ok

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,8 +1,15 @@
 // Require data here if needed
+const pokedex = require("../data/pokedex.json");
+
 
 const mainController = {
   renderHomePage: (req, res) => {
     res.render("index");
+  },
+  renderPokemonPage: (req, res) => {
+    const pokemonData = pokedex.find(pokemon => pokemon.id.toString() === req.params.pokemonId.toString());
+    console.log(pokemonData);
+    res.render("pokemon", {pokemonData});
   }
 };
 
